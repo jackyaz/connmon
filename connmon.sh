dc841e1ee9c16f3b1c293e7eba174b150a0d7226
Jack <jack.naisbett@gmail.com>
2019-07-27 19:56:51 +0100

    Fix cron delete

--- connmon.sh
+++ connmon.sh
@@ -393,9 +393,9 @@ Auto_Cron(){
 				cru d "$SCRIPT_NAME"
 			fi
 			
-			STARTUPLINECOUNTDAILY=$(cru l | grep -c "$SCRIPT_NAME daily")
-			STARTUPLINECOUNTWEEKLY=$(cru l | grep -c "$SCRIPT_NAME weekly")
-			STARTUPLINECOUNTMONTHLY=$(cru l | grep -c "$SCRIPT_NAME monthly")
+			STARTUPLINECOUNTDAILY=$(cru l | grep -c "$SCRIPT_NAME""_daily")
+			STARTUPLINECOUNTWEEKLY=$(cru l | grep -c "$SCRIPT_NAME""_weekly")
+			STARTUPLINECOUNTMONTHLY=$(cru l | grep -c "$SCRIPT_NAME""_monthly")
 			
 			if [ "$STARTUPLINECOUNTDAILY" -gt 0 ]; then
 				cru d "$SCRIPT_NAME""_daily"
