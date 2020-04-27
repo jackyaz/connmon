1193da77570b7c7c6c930192a9c49016d26e9a90
Jack <jack.naisbett@gmail.com>
2020-04-27 21:37:44 +0100

    Version bump

--- connmon.sh
+++ connmon.sh
@@ -13,7 +13,7 @@
 
 ### Start of script variables ###
 readonly SCRIPT_NAME="connmon"
-readonly SCRIPT_VERSION="v2.4.1"
+readonly SCRIPT_VERSION="v2.5.0"
 readonly SCRIPT_BRANCH="develop"
 readonly SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/""$SCRIPT_NAME""/""$SCRIPT_BRANCH"
 readonly OLD_SCRIPT_DIR="/jffs/scripts/$SCRIPT_NAME.d"
