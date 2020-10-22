0644f97134e099d3695fabfd2b55ced564148a54
Jack <jack.naisbett@gmail.com>
2020-10-22 17:28:22 +0100

    Change chart tooltip positioning

--- connmonstats_www.js
+++ connmonstats_www.js
@@ -189,9 +189,9 @@ function Draw_Chart(txtchartname,txttitle,txtunity,bordercolourname,backgroundco
 				title: function (tooltipItem, data){ return (moment(tooltipItem[0].xLabel,"X").format(timetooltipformat)); },
 				label: function (tooltipItem, data){ return round(data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].y,3).toFixed(3) + ' ' + txtunity;}
 			},
-			mode: 'x',
-			position: 'nearest',
-			intersect: false
+			mode: 'point',
+			position: 'cursor',
+			intersect: true
 		},
 		scales: {
 			xAxes: [{
