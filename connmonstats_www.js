f5e10a4dcfa490680bd480dfbd79694963888b46
Jack Naisbett <jack.naisbett@gmail.com>
2021-03-28 22:33:36 +0100

    Variable declaration in for loop

--- connmonstats_www.js
+++ connmonstats_www.js
@@ -561,8 +561,8 @@ function ToggleLines(){
 		ShowLines = "";
 		SetCookie("ShowLines","");
 	}
-	for(i = 0; i < metriclist.length; i++){
-		for(i3 = 0; i3 < 3; i3++){
+	for(var i = 0; i < metriclist.length; i++){
+		for(var i3 = 0; i3 < 3; i3++){
 			window["LineChart_"+metriclist[i]].options.annotation.annotations[i3].type=ShowLines;
 		}
 		window["LineChart_"+metriclist[i]].update();
@@ -578,15 +578,15 @@ function ToggleFill(){
 		ShowFill = "false";
 		SetCookie("ShowFill","false");
 	}
-	for(i = 0; i < metriclist.length; i++){
+	for(var i = 0; i < metriclist.length; i++){
 		window["LineChart_"+metriclist[i]].data.datasets[0].fill=ShowFill;
 		window["LineChart_"+metriclist[i]].update();
 	}
 }
 
 function RedrawAllCharts(){
-	for(i = 0; i < metriclist.length; i++){
-		for(i2 = 0; i2 < chartlist.length; i2++){
+	for(var i = 0; i < metriclist.length; i++){
+		for(var i2 = 0; i2 < chartlist.length; i2++){
 			d3.csv('/ext/connmon/csv/'+metriclist[i]+chartlist[i2]+'.htm').then(SetGlobalDataset.bind(null,metriclist[i]+chartlist[i2]));
 		}
 	}
@@ -600,7 +600,7 @@ function SetGlobalDataset(txtchartname,dataobject){
 		showhide("conntest_text", false);
 		showhide("btnRunPingtest", true);
 		BuildLastXTable();
-		for(i = 0; i < metriclist.length; i++){
+		for(var i = 0; i < metriclist.length; i++){
 			$j("#"+metriclist[i]+"_Period").val(GetCookie(metriclist[i]+"_Period","number"));
 			$j("#"+metriclist[i]+"_Scale").val(GetCookie(metriclist[i]+"_Scale","number"));
 			Draw_Chart(metriclist[i],titlelist[i],measureunitlist[i],bordercolourlist[i],backgroundcolourlist[i]);
@@ -775,7 +775,7 @@ function getChartPeriod(period){
 }
 
 function ResetZoom(){
-	for(i = 0; i < metriclist.length; i++){
+	for(var i = 0; i < metriclist.length; i++){
 		var chartobj = window["LineChart_"+metriclist[i]];
 		if(typeof chartobj === 'undefined' || chartobj === null){ continue; }
 		chartobj.resetZoom();
@@ -801,7 +801,7 @@ function ToggleDragZoom(button){
 		buttonvalue = "Drag Zoom On";
 	}
 	
-	for(i = 0; i < metriclist.length; i++){
+	for(var i = 0; i < metriclist.length; i++){
 		var chartobj = window["LineChart_"+metriclist[i]];
 		if(typeof chartobj === 'undefined' || chartobj === null){ continue; }
 		chartobj.options.plugins.zoom.zoom.drag = drag;
@@ -1154,7 +1154,7 @@ function changeAllCharts(e){
 	value = e.value * 1;
 	name = e.id.substring(0, e.id.indexOf("_"));
 	SetCookie(e.id,value);
-	for(i = 0; i < metriclist.length; i++){
+	for(var i = 0; i < metriclist.length; i++){
 		Draw_Chart(metriclist[i],titlelist[i],measureunitlist[i],bordercolourlist[i],backgroundcolourlist[i]);
 	}
 }
@@ -1211,7 +1211,7 @@ function BuildLastXTable(){
 		tablehtml+='</tr>';
 		tablehtml+='</thead>';
 		
-		for(i = 0; i < objdataname.length; i++){
+		for(var i = 0; i < objdataname.length; i++){
 			tablehtml+='<tr>';
 			tablehtml+='<td>'+moment.unix(window["DataTimestamp"][i]).format('YYYY-MM-DD HH:mm:ss')+'</td>';
 			tablehtml+='<td>'+window["DataPing"][i]+'</td>';
